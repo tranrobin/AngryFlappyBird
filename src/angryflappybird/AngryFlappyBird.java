1df3dc19c30609d84d711eb6b80bfef261175269
Linh (Jennifer) Pham <88918127+linhphamt@users.noreply.github.com>
2023-05-06 22:42:15 -0400

    Update AngryFlappyBird.java

--- src/angryflappybird/AngryFlappyBird.java
+++ src/angryflappybird/AngryFlappyBird.java
@@ -5,6 +5,7 @@ import javafx.animation.FadeTransition;
 import javafx.animation.ParallelTransition;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
